d1a415fe3c430dce9e6cdfb735b363c53c517c92
Gurpartap Dhoot <gd51731@PruCapODC.local>
2021-05-09 22:33:20 +0530

    First commit

---
+++ .profile
@@ -0,0 +1,8 @@
+export POSTGRESS_USERNAME = udagramprojectgd
+export POSTGRESS_PASSWORD = udagramprojectgd
+export POSTGRESS_DATABASE=  udagramprojectgd
+export POSTGRESS_HOST=udagramprojectgd.cwz0wloux25s.us-east-2.rds.amazonaws.com
+export AWS_REGION = us-east-2
+export AWS_PROFILE=udagramprojectgd
+export AWS_MEDIABUCKET=udagramprojectgd
+export JWT_SECRET=helloworld
---
+++ src/server.ts
@@ -0,0 +1,69 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  app.get("/filteredimage/", async (req: Request, res: Response) => {
+    let { image_url } = req.query;
+
+    if (!image_url) {
+      return res.status(400).send(`image_url is required`);
+    }
+
+    let filterimage:string;
+    try {
+      
+       filterimage = await filterImageFromURL(image_url)
+
+      await res.status(200).sendFile(filterimage, {}, (err) => {
+        if (err) { return res.status(422).send(`Not able to process the image`); }
+        // Deleting the used image file.
+        //deleteLocalFiles([filterimage])
+      })
+    } catch (err) {
+      res.status(422).send(`Not able to process the image, Make sure image url is correct`);
+    }
+    res.status(200).sendFile(filterimage,() =>{deleteLocalFiles([filterimage])});
+  });
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
